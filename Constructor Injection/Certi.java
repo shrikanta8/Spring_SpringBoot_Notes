fb18eb3707d5847cc063c42688825a75dc0dfb66
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-12-31 21:18:24 +0530

    Add files via upload

---
+++ Constructor Injection/Certi.java
@@ -0,0 +1,16 @@
+package com.skillsoft.firstSpring.ci;
+
+public class Certi {
+	 String title;
+
+	public Certi(String title) {
+		super();
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {	
+		return this.title;
+	}
+	
+}
